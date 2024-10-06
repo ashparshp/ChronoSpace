546da7b2f2adc7a2d02c0f646b7c93d1bb9ef9f8
Ashparsh Pandey <ashparsh.connects@gmail.com>
2024-10-06 11:38:09 +0530

    Recent
    Signed-off-by: Ashparsh Pandey <ashparsh.connects@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-    return (
-        <h1>MERN Blogging website by modern web</h1>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signin" element={<h1>Signin</h1>} />
+        <Route path="signup" element={<h1>Signup</h1>} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
