be92b8c95d726c708cefd1771309bee95da19b53
Ashparshp <ashparsh.connects@gmail.com>
2024-10-20 19:30:58 +0530

    2:35_Regex_Username_Email

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+import bcrypt from "bcrypt";
+import { nanoid } from "nanoid";
+
+// Importing schema
+import User from "./Schema/User.js";
 
 const server = express();
 let PORT = 3000;
@@ -14,8 +19,22 @@ mongoose.connect(process.env.DB_LOCATION, {
   autoIndex: true,
 });
 
+const generateUsername = async (email) => {
+  let username = email.split("@")[0];
+
+  let userExists = await User.exists({
+    "personal_info.username": username,
+  }).then((result) => result);
+
+  userExists ? username += nanoid().substring(0, 5) : "";
+
+  return username;
+};
+
 server.post("/signup", (req, res) => {
   let { fullname, email, password } = req.body;
+
+  email = email.toLowerCase();
 
   if (fullname.length < 3) {
     return res
@@ -27,7 +46,42 @@ server.post("/signup", (req, res) => {
     return res.status(403).json({ error: "Email is required" });
   }
 
-  return res.status(200).json({ status: "success" });
+  if (!emailRegex.test(email)) {
+    return res.status(403).json({ error: "Invalid email" });
+  }
+
+  if (!passwordRegex.test(password)) {
+    return res.status(403).json({
+      error:
+        "Password must be at least 6 characters long, and contain at least one uppercase letter, one lowercase letter, and one number",
+    });
+  }
+
+  bcrypt.hash(password, 10, async (err, hashed_password) => {
+    let username = await generateUsername(email);
+
+    let user = new User({
+      personal_info: {
+        fullname,
+        email,
+        password: hashed_password,
+        username,
+      },
+    });
+
+    user
+      .save()
+      .then((u) => {
+        return res.status(200).json({ user: u });
+      })
+      .catch((err) => {
+        console.error("MongoDB Error:", err); // Log the full error object
+        if (err.code === 11000) {
+          return res.status(500).json({ "error": "Email already exists" });
+        }
+        return res.status(500).json({ "error": err.message });
+      });
+  });
 });
 
 server.listen(PORT, () => {
