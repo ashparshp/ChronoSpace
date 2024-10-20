91afdf42946f104c90e4fef3b09633129e6925c5
Ashparshp <ashparsh.connects@gmail.com>
2024-10-20 14:11:45 +0530

    Auth_Form_UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import UserAuthForm from "./pages/userAuthForm.page";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navbar />}>
-        <Route path="signin" element={<h1>Signin</h1>} />
-        <Route path="signup" element={<h1>Signup</h1>} />
+        <Route path="signin" element={<UserAuthForm formType="sign-in"/>} />
+        <Route path="signup" element={<UserAuthForm formType="sign-up"/>} />
       </Route>
     </Routes>
   );
---
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -0,0 +1,74 @@
+import InputBox from "../components/input.component";
+import googleIcon from "../imgs/google.png";
+import { Link } from "react-router-dom";
+const UserAuthForm = ({ formType }) => {
+  return (
+    <div>
+      <section className="h-cover flex items-center justify-center">
+        <form className="w-[80%] max-w-[400px]" action="">
+          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+            {formType === "sign-in" ? "Welcome back!" : "Create an account"}
+          </h1>
+
+          {formType != "sign-in" ? (
+            <InputBox
+              name="fullname"
+              type="text"
+              placeholder="Full Name"
+              icon="fi-rr-user"
+            />
+          ) : (
+            ""
+          )}
+
+          <InputBox
+            name="email"
+            type="email"
+            placeholder="Email"
+            icon="fi-rr-envelope"
+          />
+
+          <InputBox
+            name="password"
+            type="password"
+            placeholder="Password"
+            icon="fi-rr-lock"
+          />
+
+          <button className="btn-dark center mt-14" type="submit">
+            {formType.replace("-", " ")}
+          </button>
+
+          <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+            <hr className="w-1/2 border-black" />
+            <p>or</p>
+            <hr className="w-1/2 border-black" />
+          </div>
+
+          <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+            <img src={googleIcon} className="w-5" />
+            Continue with Google
+          </button>
+
+          {
+            formType === "sign-in" ?
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Don't have an account?
+              <Link to="/signup" className="underline text-black text-xl ml-1">
+              Join us today
+              </Link>
+            </p>
+          
+          : 
+          <p className="mt-6 text-dark-grey text-xl text-center">
+              Already have an account?
+              <Link to="/signin" className="underline text-black text-xl ml-1"></Link>
+            </p>
+          }
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default UserAuthForm;
