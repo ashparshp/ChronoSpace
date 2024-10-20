491e6fc43b9275dcfa4a7c014b932a13c26fcfda
Ashparshp <ashparsh.connects@gmail.com>
2024-10-20 18:12:04 +0530

    Fixed_Search

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,17 +19,18 @@ const Navbar = () => {
             py-4 px-[5vw] md:border-0 md:block 
             md:relative md:inset-0 md:p-0 md:w-auto"
       >
-        <input
-          type="text"
-          placeholder="Search"
-          className="w-full md:w-auto bg-grey 
-                    p-4 pl-6 pr-[12%] md:pr-6 rounded-full 
-                    placeholder:text-dark-gray 
-                    md:pl-12"
-        />
-        <i className="fi fi-rr-search absolute right-[10%] md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-gray"></i>
+        <div className="relative">
+            <input
+              type="text"
+              placeholder="Search"
+              className="w-full md:w-auto bg-grey 
+                p-4 pl-6 pr-[12%] md:pr-6 rounded-full 
+                placeholder:text-dark-gray 
+                md:pl-12"
+            />
+            <i className="fi fi-rr-search absolute right-[10%] md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-gray pointer-events-none"></i>
+          </div>
       </div>
-
       <div
         className="flex items-center gap-3 
             md:gap-6 ml-auto"
