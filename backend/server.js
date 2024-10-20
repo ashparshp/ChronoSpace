486c84b6a19c3b40a189edf10fa7ab69594f59cf
Ashparshp <ashparsh.connects@gmail.com>
2024-10-20 15:06:58 +0530

    Completed_2.10

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const server = express();
+let PORT = 3000;
+
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+server.use(express.json());
+
+mongoose.connect(process.env.DB_LOCATION, {
+  autoIndex: true,
+});
+
+server.post("/signup", (req, res) => {
+  let { fullname, email, password } = req.body;
+
+  if (fullname.length < 3) {
+    return res
+      .status(403)
+      .json({ error: "Fullname must be at least 3 characters long" });
+  }
+
+  if (!email.length) {
+    return res.status(403).json({ error: "Email is required" });
+  }
+
+  return res.status(200).json({ status: "success" });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
