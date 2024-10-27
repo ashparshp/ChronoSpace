adeedd363446fdc48f158d512341cba47264ea03
Ashparshp <ashparsh.connects@gmail.com>
2024-10-27 14:58:26 +0530

    3:42

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,38 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import UserAuthForm from "./pages/userAuthForm.page";
+import { useEffect, createContext, useState } from "react";
+import { lookInSession } from "./common/session";
+
+export const UserContext = createContext({});
 
 const App = () => {
+  const [userAuth, setUserAuth] = useState({});
+
+  useEffect(() => {
+    const userInSession = lookInSession("user");
+    setUserAuth(
+      userInSession
+        ? (() => {
+            try {
+              return JSON.parse(userInSession);
+            } catch {
+              return { access_token: null };
+            }
+          })()
+        : { access_token: null }
+    );
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<Navbar />}>
-        <Route path="signin" element={<UserAuthForm formType="sign-in"/>} />
-        <Route path="signup" element={<UserAuthForm formType="sign-up"/>} />
-      </Route>
-    </Routes>
+    <UserContext.Provider value={{ userAuth, setUserAuth }}>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="signin" element={<UserAuthForm formType="sign-in" />} />
+          <Route path="signup" element={<UserAuthForm formType="sign-up" />} />
+        </Route>
+      </Routes>
+    </UserContext.Provider>
   );
 };
 
