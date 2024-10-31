ba2ba8ea10221296311740c35ed1137fbd91ccb4
Ashparshp <ashparsh.connects@gmail.com>
2024-10-31 17:00:04 +0530

    Banner

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
 import logo from "../imgs/logo.png";
+import AnimationWrapper from "../common/page-animation";
+import defaultBanner from "../imgs/blog banner.png";
 const BlogEditor = () => {
+  const handleBannerUpload = (e) => {
+    console.log(e);
+    let img = e.target.files[0];
+  };
+
   return (
     <>
       <nav className="navbar">
@@ -21,7 +28,24 @@ const BlogEditor = () => {
         </div>
       </nav>
 
-      
+      <AnimationWrapper>
+        <section>
+          <div className="mx-auto max-w-[900px] w-full">
+            <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
+              <label htmlFor="uploadBanner">
+                <img src={defaultBanner} className="z-20 " />
+                <input
+                  type="file"
+                  id="uploadBanner"
+                  hidden
+                  accept=".png, .jpg, .jpeg"
+                  onChange={handleBannerUpload}
+                />
+              </label>
+            </div>
+          </div>
+        </section>
+      </AnimationWrapper>
     </>
   );
 };
