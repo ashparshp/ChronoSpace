46d5e3a781803cd0ba6f136e13fd9f589c5e4d13
Ashparshp <ashparsh.connects@gmail.com>
2024-11-04 10:28:02 +0530

    User can edit now

--- backend/server.js
+++ backend/server.js
@@ -413,7 +413,7 @@ server.post("/search-blogs-count", (req, res) => {
 server.post("/create-blog", verifyJWT, (req, res) => {
   let authorId = req.user;
 
-  let { title, des, banner, tags, content, draft } = req.body;
+  let { title, des, banner, tags, content, draft, id } = req.body;
 
   if (!title.length) {
     return res.status(403).json({ error: "You must provide a title" });
@@ -444,52 +444,70 @@ server.post("/create-blog", verifyJWT, (req, res) => {
   tags = tags.map((tag) => tag.toLowerCase());
 
   let blog_id =
+    id ||
     title
       .replace(/[^a-zA-Z0-9]/g, " ")
       .replace(/\s+/g, "-")
       .trim() + nanoid();
 
-  let blog = new Blog({
-    title,
-    des,
-    banner,
-    content,
-    tags,
-    author: authorId,
-    blog_id: blog_id,
-    draft: Boolean(draft),
-  });
-
-  blog
-    .save()
-    .then((blog) => {
-      let incrementVal = draft ? 0 : 1;
-
-      User.findOneAndUpdate(
-        { _id: authorId },
-        {
-          $inc: { "account_info.total_posts": incrementVal },
-          $push: { blogs: blog._id },
-        }
-      )
-        .then((user) => {
-          return res.status(200).json({ id: blog.blog_id });
-        })
-        .catch((err) => {
-          return res
-            .status(500)
-            .json({ error: "Failed to update total posts number" });
-        });
-    })
-    .catch((err) => {
-      return res.status(500).json({ error: err.message });
+  if (id) {
+    Blog.findOneAndUpdate(
+      {
+        blog_id,
+      },
+      { title, des, banner, tags, content, draft: draft ? draft : false }
+    )
+      .then(() => {
+        return res.status(200).json({ id: blog_id });
+      })
+      .catch((err) => {
+        return res
+          .status(500)
+          .json({ error: "Failed to update total posts number" });
+      });
+  } else {
+    let blog = new Blog({
+      title,
+      des,
+      banner,
+      content,
+      tags,
+      author: authorId,
+      blog_id: blog_id,
+      draft: Boolean(draft),
     });
+
+    blog
+      .save()
+      .then((blog) => {
+        let incrementVal = draft ? 0 : 1;
+
+        User.findOneAndUpdate(
+          { _id: authorId },
+          {
+            $inc: { "account_info.total_posts": incrementVal },
+            $push: { blogs: blog._id },
+          }
+        )
+          .then((user) => {
+            return res.status(200).json({ id: blog.blog_id });
+          })
+          .catch((err) => {
+            return res
+              .status(500)
+              .json({ error: "Failed to update total posts number" });
+          });
+      })
+      .catch((err) => {
+        return res.status(500).json({ error: err.message });
+      });
+  }
 });
 
 server.post("/get-blog", (req, res) => {
-  let { blog_id } = req.body;
+  let { blog_id, draft, mode } = req.body;
 
-  let incrementVal = 1;
+  let incrementVal = mode != "edit" ? 1 : 0;
 
   Blog.findOneAndUpdate(
     { blog_id },
@@ -507,6 +525,11 @@ server.post("/get-blog", (req, res) => {
       ).catch((err) => {
         return res.status(500).json({ error: err.message });
       });
+
+      if (blog.draft && !draft) {
+        return res.status(500).json({ error: "You can't access draft blog" });
+      }
+
       return res.status(200).json({ blog });
     })
     .catch((err) => {
--- frontend/src/pages/editor.pages.jsx
+++ frontend/src/pages/editor.pages.jsx
@@ -1,8 +1,10 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { UserContext } from "../App";
-import { Navigate } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import BlogEditor from "../components/blog-editor.component";
 import PublishForm from "../components/publish-form.component";
+import Loader from "../components/loader.component";
+import axios from "axios";
 
 const blogStructure = {
   title: "",
@@ -16,11 +18,37 @@ const blogStructure = {
 export const EditorContext = createContext({});
 
 const Editor = () => {
+  const { blog_id } = useParams();
+
   const [blog, setBlog] = useState(blogStructure);
 
   const [editorState, setEditorState] = useState("editor");
 
   const [textEditor, setTextEditor] = useState({ isReady: false });
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!blog_id) {
+      setLoading(false);
+      return;
+    }
+
+    axios
+      .post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", {
+        blog_id,
+        draft: true,
+        mode: "edit",
+      })
+      .then(({ data: { blog } }) => {
+        setBlog(blog);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setBlog(null);
+        setLoading(false);
+      });
+  }, []);
 
   let {
     userAuth: { access_token },
@@ -39,6 +67,8 @@ const Editor = () => {
     >
       {access_token === null ? (
         <Navigate to="/signin" />
+      ) : loading ? (
+        <Loader />
       ) : editorState === "editor" ? (
         <BlogEditor />
       ) : (
